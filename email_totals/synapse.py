889b8f79eb030f9e43a15231a82944c46461c51c
Joni Harker <joni.harker@sagebase.org>
2023-07-10 17:30:03 -0700

    Add Synapse client configuration
    Add parameters for the Synapse domain and team ID.

--- email_totals/synapse.py
+++ email_totals/synapse.py
@@ -1,12 +1,10 @@
 import logging
+import os
 
 import synapseclient as syn
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
-
-synapse_email = '@synapse.org'
-synapse_team_sage = '273957'
 
 # Create a Synapse client
 # The lambda homedir is not writeable, put the cache in /tmp
@@ -18,12 +16,15 @@ def get_team_sage_members():
     Get a list of Team Sage emails from Synapse
     """
 
+    synapse_id = os.environ['SYNAPSE_TEAM_ID']
+    synapse_domain = os.environ['SYNAPSE_TEAM_DOMAIN']
+
     team_sage = []
 
-    syn_team = syn_client.getTeam(synapse_team_sage)
+    syn_team = syn_client.getTeam(synapse_id)
     syn_members = syn_client.getTeamMembers(syn_team)
     for m in syn_members:
-        email = m['member']['userName'] + synapse_email
+        email = m['member']['userName'] + synapse_domain
         team_sage.append(email)
 
     LOG.info(f"Members of Team Sage: {team_sage}")
--- tests/unit/test_synapse.py
+++ tests/unit/test_synapse.py
@@ -1,7 +1,15 @@
+import os
+
 from email_totals import synapse
 
 
 def test_team_sage(mocker, mock_syn_team, mock_syn_members, mock_team_sage):
+    env_vars = {
+        'SYNAPSE_TEAM_ID': '123',
+        'SYNAPSE_TEAM_DOMAIN': '@synapse.org',
+    }
+    mocker.patch.dict(os.environ, env_vars)
+
     mock_syn_client = mocker.MagicMock(spec=synapse.syn_client)
 
     mock_syn_client.getTeam.return_value = mock_syn_team
